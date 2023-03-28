819088658306d081d9ad55173974213f5d436bf4
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-03-28 13:39:00 -0300

    aula 01 - desafio 02

--- src/App.java
+++ src/App.java
@@ -7,6 +7,11 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+
+	public static final String ANSI_RED = "\u001B[41m";
+	public static final String ANSI_REGULAR = "\u001B[m";
+	public static final String ANSI_BOLD = "\u001B[1m";
+
 	public static void main(String[] args) throws Exception {
 		// fazer uma conexão http e buscar o top 250
 		String urlTopMovies = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
@@ -15,19 +20,33 @@ public class App {
 
 		HttpRequest request = HttpRequest.newBuilder(address).GET().build();
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-		
+
 		// extrair so os dados que interessam (titulo, poster, classificação)
 		String body = response.body();
 		JsonParser parser = new JsonParser();
 		List<Map<String, String>> filmList = parser.parse(body);
-		
-		// exibir e manipular os dados 
-		for(Map<String, String> film : filmList) {
-			System.out.println(film.get("title"));
-			System.out.println(film.get("image"));
-			System.out.println(film.get("imDbRating"));
+
+		// exibir e manipular os dados
+		for (Map<String, String> film : filmList) {
+			System.out.println(ANSI_BOLD + "Title: " + ANSI_REGULAR + film.get("title"));
+			System.out.println(ANSI_BOLD + "URL image: " + ANSI_REGULAR + film.get("image"));
+
+			double rating = Double.parseDouble(film.get("imDbRating"));
+			System.out.print(ANSI_BOLD + "Rating: " + rating + " ");
+
+			int stars = (int) rating;
+			for (int i = 0; i < stars; i++) {
+				System.out.print(ANSI_RED + "❤️");
+			}
+
+			int emptyStars = 10 - stars;
+			for (int i = 0; i < emptyStars; i++) {
+				System.out.print("🤍");
+			}
+
+			System.out.println(ANSI_REGULAR);
 			System.out.println();
 		}
-		
+
 	}
 }
